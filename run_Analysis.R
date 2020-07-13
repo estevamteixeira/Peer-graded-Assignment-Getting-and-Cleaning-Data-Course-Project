15d917f4b04540bd8bcbee02a02137923ba9dd21
Estevam Caixeta Martins Teixeira <12239379+estevamteixeira@users.noreply.github.com>
2020-07-13 16:39:21 -0400

    Add files via upload

--- run_Analysis.R
+++ run_Analysis.R
@@ -33,14 +33,76 @@ subject_train <- read.table(file.path(path,"train","subject_train.txt"),
 x_test <- read.table(file.path(path,"test","X_test.txt"), header = FALSE)
 y_test <- read.table(file.path(path,"test","y_test.txt"), header = FALSE)
 subject_test <- read.table(file.path(path,"test","subject_test.txt"),
-                            header = FALSE)
+                           header = FALSE)
+
+##-----------------------------------------------------------------
+## 1. Merges the training and the test sets to create one data set.
+##-----------------------------------------------------------------
+
+subject <- rbind(subject_train, subject_test)
+x <- rbind(x_train, x_test)
+y <- rbind(y_train, y_test)
 
 
+## naming the variables
+
+names(subject) <- c("subject")
+names(y) <- c("activity")
+x_names <- read.table(file.path(path,"features.txt"), header = FALSE)
+names(x) <- x_names$V2
+
+## combining
+
+data <- cbind(subject, y, x)
+
+##---------------------------------------------------------------------
+## 2.Extracts only the measurements on the mean and standard deviation 
+## for each measurement.
+##---------------------------------------------------------------------
+
+## match the arguments we want to subset
+idx <- x_names$V2[grep("mean\\(.*\\)|std\\(.*\\)", x_names$V2)]
+idx <- c("subject","activity", idx)
+
+## subset the dataset
+subs <- data[,names(data) %in% idx == TRUE]
+head(subs)
 
 
+##--------------------------------------------------------------------------
+## 3. Uses descriptive activity names to name the activities in the data set
+##--------------------------------------------------------------------------
 
+require(dplyr)
 
+activityLabels <- read.table(file.path(path,"activity_labels.txt"), header = FALSE)
+head(activityLabels)
 
+# merge both datasets and bring activity names
+subs <- merge(subs, activityLabels, by.x = "activity", by.y="V1")
+names(subs)[69] <- "activityLabels"
+
+##----------------------------------------------------------------------
+## 4. Appropriately labels the data set with descriptive variable names.
+##----------------------------------------------------------------------
+
+names(subs) <- gsub("^t", "time", names(subs))
+names(subs) <- gsub("^f", "frequency", names(subs))
+names(subs) <- gsub("Acc", "Accelerometer", names(subs))
+names(subs) <- gsub("Gyro", "Gyroscope", names(subs))
+names(subs) <- gsub("Mag", "Magnitude", names(subs))
+names(subs) <- gsub("BodyBody", "Body", names(subs))
+
+##---------------------------------------------------------------------------
+## 5.From the data set in step 4, creates a second, independent tidy data set
+## with the average of each variable for each activity and each subject.
+##---------------------------------------------------------------------------
+
+ind_tidy_data <- subs %>%
+    group_by(activity, subject) %>%
+    summarise_each(funs(mean), -activityLabels) %>%
+    ungroup() %>%
+    arrange(activity, subject)
 
 
 
